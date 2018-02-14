a851fae9e0ee4e8d5b579e21aeef3bea91c16ca6
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2018-02-14 12:28:36 +0700

    Create Index.php

---
+++ controllers/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace controllers;
+use libs\Controller;
+
+class Index extends Controller {
+
+	function __construct(){
+		parent::__construct();
+	}
+
+	function actionIndex(){
+		$this->view->title = "Index";
+		return $this->view->render("index",1);
+	}
+	
+
+}
