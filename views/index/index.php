07484841ed0bf52e56c829726bad29e789b52af1
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2018-02-14 12:32:01 +0700

    Create index.php

---
+++ views/index/index.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="jumbotron transparent">
+		<div class="container">
+			<h2>Selamat Datang di MVC</h2>
+			<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+		</div>
+	</div>
+</div>
