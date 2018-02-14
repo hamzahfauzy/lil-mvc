33a466d6512e1b838d61227f103ac03ad37deda3
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2018-02-14 12:30:15 +0700

    Add files via upload

---
+++ libs/View.php
@@ -0,0 +1,31 @@
+<?php
+namespace libs;
+/**
+* View Class
+*/
+class View 
+{
+	
+	function __construct()
+	{
+		$vendor = require_once("config/vendor.php");
+		$this->vendor = $vendor;
+	}
+
+	function render($file, $layout = false, $data = false){
+		if($data==true){
+			if(is_array($data)){
+				extract($data);
+			}else{
+				return Bootloader::error500();
+			}
+		}
+		if($layout==true){
+			require 'views/layouts/header.php';
+			require 'views/'.$this->controller.'/'.$file.'.php';
+			require 'views/layouts/footer.php';
+		}else{
+			require 'views/'.$this->controller.'/'.$file.'.php';
+		}
+	}
+}
